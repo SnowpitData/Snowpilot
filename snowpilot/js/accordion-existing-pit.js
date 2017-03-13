62d8fd7cd0cc843a5749062ab04f274f45a07834
jdebr <jd.debr@gmail.com>
2017-03-12 20:07:04 -0600

    Work on accordion layers

--- snowpilot/js/accordion-existing-pit.js
+++ snowpilot/js/accordion-existing-pit.js
@@ -1,25 +1,32 @@
 		 
 (function ($) {
 
-		   // Behaviors related to Snowpit Profile Forms
-	Drupal.behaviors.snowpilot3 = {
+  // Behaviors related to Snowpit Profile Forms
+  Drupal.behaviors.snowpilot3 = {
 
 	attach: function (context, settings) {
- 		 $('.form-field-type-field-collection table#field-layer-values > tbody').accordion({
+		
+		var tableSelector = "table[id^='field-layer-values'] > tbody";
+		
+ 		//$('.form-field-type-field-collection table#field-layer-values > tbody').accordion({
+		$(tableSelector).accordion({
  			 header: "h3",
  			 collapsible: true,
  			 active: false,
 			 heightStyle: "content"
- 		 });
+ 		});
 		 
-     
-     $('.horizontal-tabs-panes').once('accordion_ajax_toggle', function() {
-         $( '.form-field-type-field-collection table#field-layer-values > tbody').accordion('option', 'active', ":last");
-       $(document).ajaxComplete(function() {
-         $( '.form-field-type-field-collection table#field-layer-values > tbody').accordion('option', 'active', ":last");
-       });
-     });
-	
-	}
-	}
+		$('.horizontal-tabs-panes').once('accordion_ajax_toggle', function() {
+			//$( '.form-field-type-field-collection table#field-layer-values > tbody').accordion('option', 'active', -1);
+			$(tableSelector).accordion('option', 'active', -1);
+			
+			$(document).ajaxComplete(function() {
+				 //$( '.form-field-type-field-collection table#field-layer-values > tbody').accordion('option', 'active', -1);
+				 $(tableSelector).accordion('option', 'active', -1);
+			});
+			
+		});	
+		
+	} // end attach 
+  } // end Drupal.behaviors.snowpilot3
 }) (jQuery);
