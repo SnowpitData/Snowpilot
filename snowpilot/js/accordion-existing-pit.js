b9949c52f9789ce4732a0645f613fea5d07ef2e2
jdebr <jd.debr@gmail.com>
2018-02-15 15:19:13 -0700

    Split up accordion selectors

--- snowpilot/js/accordion-existing-pit.js
+++ snowpilot/js/accordion-existing-pit.js
@@ -5,18 +5,51 @@
   Drupal.behaviors.snowpilot3 = {
 
 	attach: function (context, settings) {
-		
-		$( '.form-field-type-field-collection table.field-multiple-table > tbody').accordion({
+		// Activate accordian functionality on all 4 tables
+		$( '#edit-field-layer table.field-multiple-table > tbody').accordion({
  			 header: "h3",
  			 collapsible: true,
  			 active: false,
 			 heightStyle: "content"
  		});
+    
+    $( '#edit-field-test table.field-multiple-table > tbody').accordion({
+ 			 header: "h3",
+ 			 collapsible: true,
+ 			 active: false,
+			 heightStyle: "content"
+ 		});
+    
+    /*
+    $( '#edit-field-temp-collection table.field-multiple-table > tbody').accordion({
+ 			 header: "h3",
+ 			 collapsible: true,
+ 			 active: false,
+			 heightStyle: "content"
+ 		});
+    */
+    
+    /*
+    $( '#edit-field-density-profile table.field-multiple-table > tbody').accordion({
+ 			 header: "h3",
+ 			 collapsible: true,
+ 			 active: false,
+			 heightStyle: "content"
+ 		});
+    */
 		 
+    // Set the final layer of each table as active, and attach ajax listener to keep it that way
 		$('.horizontal-tabs-panes').once('accordion_ajax_toggle', function() {
-			$( '.form-field-type-field-collection table.field-multiple-table > tbody').accordion('option', 'active', -1);
+			$( '#edit-field-layer table.field-multiple-table > tbody').accordion('option', 'active', -1);
+      $( '#edit-field-test table.field-multiple-table > tbody').accordion('option', 'active', -1);
+      //$( '#edit-field-temp-collection table.field-multiple-table > tbody').accordion('option', 'active', -1);
+      //$( '#edit-field-density-profile table.field-multiple-table > tbody').accordion('option', 'active', -1);
+      // AJAX listener
 			$(document).ajaxComplete(function() {
-				$( '.form-field-type-field-collection table.field-multiple-table > tbody').accordion('option', 'active', -1);
+				$( '#edit-field-layer table.field-multiple-table > tbody').accordion('option', 'active', -1);
+        $( '#edit-field-test table.field-multiple-table > tbody').accordion('option', 'active', -1);
+        //$( '#edit-field-temp-collection table.field-multiple-table > tbody').accordion('option', 'active', -1);
+        //$( '#edit-field-density-profile table.field-multiple-table > tbody').accordion('option', 'active', -1);
 			});
 		});
 		
